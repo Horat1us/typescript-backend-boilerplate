d519fff446a4404b0ce03aacc825ae07178fa82f
Alexander horat1us Letnikow <reclamme@gmail.com>
2017-11-04 00:20:35 +0300

    Initial commit

---
+++ tests/example-specs.ts
@@ -0,0 +1,8 @@
+import {expect} from "chai";
+
+describe("Sample test", () => {
+    it("should run test", () => {
+        const result = 1 + 1;
+        expect(result).to.be.equal(2);
+    });
+});
